17d0f598c09dcb86120488cf9d7b47517cebd5ae
Faraz Mehraien <farazmehraien@yahoo.com>
2019-09-23 09:36:27 +0330

    Updates

---
+++ precache-manifest.4d3b416b178527bded281484ec5fafd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef67fea0ab76005c4e38eff8772fb18a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6de7e8df2ffad882d30",
+    "url": "/static/css/main.4bf4f1af.chunk.css"
+  },
+  {
+    "revision": "e80ca8ffe1a8cf196282",
+    "url": "/static/js/2.53568b1f.chunk.js"
+  },
+  {
+    "revision": "d6de7e8df2ffad882d30",
+    "url": "/static/js/main.2e3ed127.chunk.js"
+  },
+  {
+    "revision": "be99d0b5df09de2e2cdf",
+    "url": "/static/js/runtime-main.b6d495d2.js"
+  }
+]);
